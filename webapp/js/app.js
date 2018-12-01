752ae4b4fe0ae725b24a22d51ef359362ea088c5
inux <inux.steve@gmail.com>
2018-12-02 00:39:28 +0100

    small corrections, added objloader (missed dependency)

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -1,8 +1,17 @@
+//Global variables
+var content = document.getElementById("content");
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, content.innerWidth / content.innerHeight, 0.1, 1000);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(content.innerWidth, content.innerHeight);
+content.appendChild(renderer.domElement);
+
 function loadJson(path, callback) {
     var url = path;
     var xmlhttp = new XMLHttpRequest();
 
-    xmlhttp.onreadystatechange = function() {
+    xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var data = JSON.parse(this.responseText);
             callback(data);
@@ -25,10 +34,29 @@ function createObjSelection(config) {
         slct.options.add(option);
     });
     document.getElementById("objselection").append(slct);
+    openObj(slct.options[0].value);
 }
 
-function selectObj(objPath) {
-    console.log(objPath);
+function selectObj(event) {
+    openObj(event.target.value);
+}
+
+function openObj(objPath) {
+    console.log("Opening Obj -> " + objPath);
+
+    var loader = new THREE.OBJLoader();
+    loader.load(
+        objPath,
+        function (object) {
+            scene.add(object);
+        },
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+        },
+        function (error) {
+            console.error(error);
+        }
+    );
 }
 
 //Start doing work...
