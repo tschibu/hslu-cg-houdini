bea698650d8e6901e3c2b85e107415ceee302068
inux <inux.steve@gmail.com>
2018-12-02 01:00:11 +0100

    first working loader

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -1,11 +1,39 @@
 //Global variables
+var mouseX = 0, mouseY = 0;
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
 var content = document.getElementById("content");
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, content.innerWidth / content.innerHeight, 0.1, 1000);
-
+var ambientLight = new THREE.AmbientLight(0xcccccc, 0.4);
+var pointLight = new THREE.PointLight(0xffffff, 0.8);
 var renderer = new THREE.WebGLRenderer();
+
+//create scene
+scene.add(ambientLight);
+camera.add(pointLight);
+scene.add(camera);
+
+//renderer
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(content.innerWidth, content.innerHeight);
 content.appendChild(renderer.domElement);
+
+//event listeners
+document.addEventListener('mousemove', onDocumentMouseMove, false);
+window.addEventListener('resize', onWindowResize, false);
+
+
+//Start doing work...
+
+//load config and create obj selection
+loadJson('/config.json', createObjSelection);
+
+animate();
+
+//-----------------------------------------------------------------
+// Functions
 
 function loadJson(path, callback) {
     var url = path;
@@ -48,6 +76,7 @@ function openObj(objPath) {
     loader.load(
         objPath,
         function (object) {
+            console.log(object);
             scene.add(object);
         },
         function (xhr) {
@@ -59,7 +88,29 @@ function openObj(objPath) {
     );
 }
 
-//Start doing work...
+function onWindowResize() {
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = window.innerHeight / 2;
 
-//load config and create obj selection
-loadJson('/config.json', createObjSelection);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function onDocumentMouseMove(event) {
+    mouseX = (event.clientX - windowHalfX) / 2;
+    mouseY = (event.clientY - windowHalfY) / 2;
+}
+
+function render() {
+    camera.position.x += (mouseX - camera.position.x) * .05;
+    camera.position.y += (- mouseY - camera.position.y) * .05;
+    camera.lookAt(scene.position);
+    renderer.render(scene, camera);
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    render();
+}
