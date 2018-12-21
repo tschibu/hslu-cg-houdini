46ca44d32297224f38b76e193fad32635b983095
inux <inux.steve@gmail.com>
2018-12-21 02:53:32 +0100

    design and navigation

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -1,5 +1,21 @@
 //Global variables
-var mouseX = 0, mouseY = 0;
+var moveSpeed = 1;
+var rotationSpeed = 0.02;
+var maxSliderValue = 15;
+var moveForward = false;
+var moveBackward = false;
+var moveLeft = false;
+var moveRight = false;
+var moveUp = false;
+var moveDown = false;
+var rotLeft = false;
+var rotRight = false;
+var rotUp = false;
+var rotDown = false;
+var velocityMove = new THREE.Vector3();
+var velocityRot = new THREE.Vector3();
+var prevTime = performance.now();
+
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
@@ -14,29 +30,37 @@ var size = 1000;
 var divisions = 100;
 var helperGrid = new THREE.GridHelper(size, divisions);
 
-//create scene
-scene.add(ambientLight);
+//scene
+createScene();
+
+//camera
 camera.add(pointLight);
-scene.add(camera);
 camera.position.x = 0;
 camera.position.y = 18;
 camera.position.z = 84;
-
-//add helper grid
-scene.add(helperGrid);
 
 //renderer
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(content.innerWidth, content.innerHeight);
 content.appendChild(renderer.domElement);
 
-//event listeners
-//document.addEventListener('mousemove', onDocumentMouseMove, false);
-window.addEventListener('resize', onWindowResize, false);
-window.addEventListener('keypress', onDocumentKeyPress, false);
-
-
+/*******************************************************************/
 //Start doing work...
+/*******************************************************************/
+
+//event listeners
+//document  .addEventListener('mousemove', onDocumentMouseMove, false);
+window.addEventListener('resize', onWindowResize, false);
+window.addEventListener('keydown', onKeyDown, false);
+window.addEventListener('keyup', onKeyUp, false);
+document.getElementById("moveSpeed").oninput = function () {
+    //input range: 0 - 15 -> maxSliderValue
+    moveSpeed = this.value;
+}
+document.getElementById("rotationSpeed").oninput = function () {
+    //input range: 0 - 15 -> maxSliderValue
+    rotationSpeed = this.value;
+}
 
 //load config and create obj selection
 loadJson('/config.json', createObjSelection);
@@ -46,30 +70,37 @@ animate();
 //-----------------------------------------------------------------
 // Functions
 
-function render() {
-    //camera.position.x += (mouseX - camera.position.x) * .05;
-    //camera.position.y += (- mouseY - camera.position.y) * .05;
-    //camera.lookAt(scene.position);
-    renderer.render(scene, camera);
-}
-
 function animate() {
     requestAnimationFrame(animate);
     render();
+    updateData();
 }
 
-function loadJson(path, callback) {
-    var url = path;
-    var xmlhttp = new XMLHttpRequest();
+function render() {
+    renderer.render(scene, camera);
+    THREE.Utils.cameraLookDir(camera);
+    moveCamera();
+}
 
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var data = JSON.parse(this.responseText);
-            callback(data);
-        }
-    };
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
+function updateData() {
+    //Camera
+    document.getElementById("posX").innerText = "Pos X: " + toFixed(camera.position.x);
+    document.getElementById("posY").innerText = "Pos Y: " + toFixed(camera.position.y);
+    document.getElementById("posZ").innerText = "Pos Z: " + toFixed(camera.position.z);
+    document.getElementById("rotX").innerText = "Rot X: " + toFixed(camera.rotation.x);
+    document.getElementById("rotY").innerText = "Rot Y: " + toFixed(camera.rotation.y);
+    document.getElementById("rotZ").innerText = "Rot Z: " + toFixed(camera.rotation.z);
+}
+
+function createScene() {
+    //create scene
+    scene = new THREE.Scene();
+
+    scene.add(ambientLight);
+    scene.add(camera);
+
+    //add helper grid
+    scene.add(helperGrid);
 }
 
 function createObjSelection(config) {
@@ -107,6 +138,7 @@ function openObj(objPath) {
         function (object) {
             console.log("Add object to Scene:");
             console.log(object);
+            createScene();
             scene.add(object);
         },
         function (xhr) {
@@ -128,62 +160,155 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function onDocumentKeyPress( event ) {
-    var keyCode = event.which;
-    var positionDelta = 6;
-    var rotationDelta = 0.1;
-    console.log(keyCode);
-    //A
-    if ( keyCode == 97 )
-    {
-        camera.position.x -= positionDelta;
-    }
-    //D
-    else if ( keyCode == 100 )
-    {
+function moveCamera() {
+    var time = performance.now();
+    var delta = ( time - prevTime ) / 1000;
 
-        camera.position.x += positionDelta;
+    speedInit = (30 / maxSliderValue) * moveSpeed;
+    speed = (4500.0 / maxSliderValue) * moveSpeed;
+
+    velocityMove.x -= velocityMove.x * speedInit * delta;
+    if ( moveLeft ) velocityMove.x -= speed * delta;
+    if ( moveRight ) velocityMove.x += speed * delta
+
+    velocityMove.y -= velocityMove.y * speedInit * delta;
+    if ( moveDown ) velocityMove.y -= speed * delta;
+    if ( moveUp ) velocityMove.y += speed * delta;
+
+    velocityMove.z -= velocityMove.z * speedInit * delta;
+    if ( moveForward ) velocityMove.z -= speed * delta;
+    if ( moveBackward ) velocityMove.z += speed * delta;
+
+    //pass velocity as an argument to translateZ and call it on camera.
+    camera.translateX( velocityMove.x * delta );
+    camera.translateY( velocityMove.y * delta );
+    camera.translateZ( velocityMove.z * delta );
+
+    rotInit = (3 / maxSliderValue) * rotationSpeed;
+    rot = (2000 / maxSliderValue) * rotationSpeed;
+
+    camera.rotation.x -= camera.rotation.x * rotInit * delta;
+    if ( rotDown ) camera.rotation.x -= rot * delta;
+    if ( rotUp ) camera.rotation.x += rot * delta
+
+    camera.rotation.y -= camera.rotation.y * rotInit * delta;
+    if ( rotRight ) camera.rotation.y -= rot * delta;
+    if ( rotLeft ) camera.rotation.y += rot * delta;
+
+    prevTime = time;
+}
+
+
+function onKeyDown(event) {
+    var keyCode = event.which;
+    switch (keyCode) {
+        case 87:  // W
+        case 119: // w
+            moveForward = true;
+            break;
+        case 65:  // A
+        case 97:  // a
+            moveLeft = true;
+            break;
+        case 68:  // d
+        case 100: // D
+            moveRight = true;
+            break;
+        case 83:  // s
+        case 115: // S
+            moveBackward = true;
+            break;
+        case 81:  // q
+        case 113: // Q
+            moveUp = true;
+            break;
+        case 69:  // e
+        case 101: // E
+            moveDown = true;
+            break;
+        case 84:  // t
+        case 116: // T
+            rotUp = true;
+            break;
+        case 71:  // g
+        case 103: // G
+            rotDown = true;
+            break;
+        case 70:  // f
+        case 102: // F
+            rotLeft = true;
+            break;
+        case 72:  // h
+        case 104: // H
+            rotRight = true;
+            break;
     }
-    //W
-    else if ( keyCode == 119 )
-    {
-        camera.position.z -= positionDelta;
+    console.log({"w": moveForward, "a": moveLeft, "d": moveRight, "s": moveBackward, "q": moveUp, "e": moveDown, "t": rotUp, "f": rotLeft, "h": rotRight, "g": rotDown});
+}
+
+function onKeyUp(event) {
+    var keyCode = event.which;
+    switch (keyCode) {
+        case 87:  // W
+        case 119: // w
+            moveForward = false;
+            break;
+        case 65:  // A
+        case 97:  // a
+            moveLeft = false;
+            break;
+        case 68:  // d
+        case 100: // D
+            moveRight = false;
+            break;
+        case 83:  // s
+        case 115: // S
+            moveBackward = false;
+            break;
+        case 81:  // q
+        case 113: // Q
+            moveUp = false;
+            break;
+        case 69:  // e
+        case 101: // E
+            moveDown = false;
+            break;
+        case 84:  // t
+        case 116: // T
+            rotUp = false;
+            break;
+        case 71:  // g
+        case 103: // G
+            rotDown = false;
+            break;
+        case 70:  // f
+        case 102: // F
+            rotLeft = false;
+            break;
+        case 72:  // h
+        case 104: // H
+            rotRight = false;
+            break;
     }
-    //S
-    else if ( keyCode == 115 )
-    {
-        camera.position.z += positionDelta;
-    }
-    //Q
-    else if ( keyCode == 113 )
-    {
-        camera.position.y += positionDelta;
-    }
-    //E
-    else if ( keyCode == 101 )
-    {
-        camera.position.y -= positionDelta;
-    }
-    //T
-    else if ( keyCode == 116 )
-    {
-        camera.rotation.x += rotationDelta;
-    }
-    //G
-    else if ( keyCode == 103 )
-    {
-        camera.rotation.x -= rotationDelta;
-    }
-    //F
-    else if ( keyCode == 102 )
-    {
-        camera.rotation.y += rotationDelta;
-    }
-    //H
-    else if ( keyCode == 104 )
-    {
-        camera.rotation.y -= rotationDelta;
-    }
-    console.log(camera.position);
-    console.log(camera.rotation);
+    console.log({"w": moveForward, "a": moveLeft, "d": moveRight, "s": moveBackward, "q": moveUp, "e": moveDown, "t": rotUp, "f": rotLeft, "h": rotRight, "g": rotDown});
+}
+
+// Helper Functions
+
+function loadJson(path, callback) {
+    var url = path;
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            callback(data);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+function toFixed(number) {
+    return Number((number).toFixed(2));
 }
---
+++ webapp/js/three-utils.js
@@ -0,0 +1,7 @@
+THREE.Utils = {
+    cameraLookDir: function(camera) {
+        var vector = new THREE.Vector3(0, 0, -1);
+        vector.applyEuler(camera.rotation, camera.rotation.rotation);
+        return vector;
+    }
+};
